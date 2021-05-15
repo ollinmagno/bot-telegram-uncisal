9da6c149694fba4e56718aa6b1e5cb387dc51454
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2021-05-15 00:05:06 -0300

    Add files via upload

---
+++ src/test/java/br/com/bot/uncisal/UncisalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bot.uncisal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncisalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
